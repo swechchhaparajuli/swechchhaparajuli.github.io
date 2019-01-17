e8e146d80ff4af9e1237836c4bfe2e6173ae25fd
swechchhaparajuli <swechchha@gmail.com>
2019-01-16 21:04:27 -0800

    linked navigation to js

--- js/styles.js
+++ js/styles.js
@@ -1,2 +1,22 @@
 
 
+
+$(document).on("click", ".home", function(){
+    console.log("home clicked");
+    window.location.href =  "./index.html";
+});
+
+$(document).on("click", ".artPortfolio", function(){
+    window.location.href =  "./portfolio.html";
+
+});
+
+$(document).on("click", ".contact", function(){
+    window.location.href =  "./contact.html";
+
+});
+
+$(document).on("click", ".webdevPortfolio", function(){
+    window.location.href =  "./portfolio.html";
+
+});
