4e6385c2445bdc9692c9042746237016a8249bf9
swechchhaparajuli <swechchha@gmail.com>
2019-01-22 09:21:42 -0800

    added a portfolio link

--- js/styles.js
+++ js/styles.js
@@ -1,22 +1,23 @@
 
 
+var profileDescription = document.getElementById("profile-description");
+var profileImage = document.getElementById("profile-image");
+
+profileDescription.textContent = " Hi! I'm Swechchha Parajuli. I am currently an undergraduate student at University of California, Santa Cruz. I'm majoring in Computer Science."
 
 $(document).on("click", ".home", function(){
     console.log("home clicked");
     window.location.href =  "./index.html";
-});
+});  
 
 $(document).on("click", ".artPortfolio", function(){
     window.location.href =  "./artPortfolio.html";
-
 });
 
 $(document).on("click", ".contact", function(){
     window.location.href =  "./contact.html";
-
 });
 
 $(document).on("click", ".webdevPortfolio", function(){
     window.location.href =  "./webdevPortfolio.html";
-
 });
