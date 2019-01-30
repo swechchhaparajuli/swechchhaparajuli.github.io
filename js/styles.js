93356531f39036f6670ad7ede71ac61e436774f6
swechchhaparajuli <swechchha@gmail.com>
2019-01-29 16:05:48 -0800

    new website

--- js/styles.js
+++ js/styles.js
@@ -3,26 +3,30 @@
 var profileDescription = document.getElementById("profile-description");
 var profileImage = document.getElementById("profile-image");
 
-profileDescription.textContent = " Hi! I'm Swechchha Parajuli. I am currently an undergraduate student at University of California, Santa Cruz. I'm majoring in Computer Science."
+profileDescription.textContent = " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ante sem, lacinia eget ante sit amet, tincidunt ultricies velit. Fusce quis sagittis dui. Cras lobortis ipsum urna, sit amet molestie ligula condimentum vitae. Sed feugiat urna vitae leo ultrices finibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In hac habitasse platea dictumst. Pellentesque mi massa, viverra at rutrum et, gravida eu metus. Nam hendrerit nisl id erat tincidunt, vitae rhoncus odio fringilla. Suspendisse id nibh id ex faucibus ultrices. Aenean tincidunt mattis massa vel commodo. Nulla at nisi cursus, cursus enim vitae, tempus velit. Nulla facilisi." +
 
+"Vivamus a ullamcorper velit. Pellentesque lorem orci, tincidunt sit amet dui a, lacinia imperdiet lacus. Vivamus at ex eget diam placerat venenatis non et tortor. Aenean lacinia venenatis augue, vitae convallis justo consectetur vel. Vivamus quis vestibulum ex. Donec ante neque, vestibulum nec lorem a, porttitor pretium elit. Cras ac nisl elit.\n" +
+
+"Donec sagittis massa non mauris aliquet, sit amet rhoncus orci facilisis. Donec a odio nunc. Suspendisse in aliquet diam. Etiam accumsan scelerisque enim eget suscipit. Curabitur commodo metus eget tristique imperdiet. Aliquam malesuada enim libero, in laoreet nisl tincidunt dapibus. Sed finibus non orci eu egestas. Sed dapibus velit vitae suscipit ornare. Donec est massa, imperdiet sagittis dolor vel, luctus accumsan lectus. Nulla facilisi. Maecenas egestas pretium erat, non bibendum metus facilisis sit amet. Integer faucibus in sapien eget aliquam. Maecenas mauris orci, pretium non condimentum non, fringilla quis justo. Nam ultricies, metus et egestas gravida, eros magna finibus turpis, quis posuere mauris felis nec augue. Nullam dui ipsum, faucibus a feugiat sit amet, pretium cursus turpis."
+
+/*
 $(document).on("click", ".home", function(){
     console.log("home clicked");
     window.location.href =  "./index.html";
 }); 
 
-$(document).on("click", "#mongoScraper", function(){
-    console.log("scrape clicked");
-    window.location.href =  "http://swechchha.com/clickyGame/";
-});   
+$(document).on("click", ".contact", function(){
+    window.location.href =  "./contact.html";
+});
+
+
 
 $(document).on("click", ".artPortfolio", function(){
     window.location.href =  "./artPortfolio.html";
 });
 
-$(document).on("click", ".contact", function(){
-    window.location.href =  "./contact.html";
-});
+
 
 $(document).on("click", ".webdevPortfolio", function(){
     window.location.href =  "./webdevPortfolio.html";
-});
+});*/
