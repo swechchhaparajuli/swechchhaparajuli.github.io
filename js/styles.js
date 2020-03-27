63b6972354be27aae6d6b0c604a39a8924d1e780
swechchhaparajuli <swechchha@gmail.com>
2020-03-27 15:08:01 -0700

    responsiveness fixed

--- js/styles.js
+++ js/styles.js
@@ -9,6 +9,8 @@ profileDescription.textContent = " Lorem ipsum dolor sit amet, consectetur adipi
 
 "Donec sagittis massa non mauris aliquet, sit amet rhoncus orci facilisis. Donec a odio nunc. Suspendisse in aliquet diam. Etiam accumsan scelerisque enim eget suscipit. Curabitur commodo metus eget tristique imperdiet. Aliquam malesuada enim libero, in laoreet nisl tincidunt dapibus. Sed finibus non orci eu egestas. Sed dapibus velit vitae suscipit ornare. Donec est massa, imperdiet sagittis dolor vel, luctus accumsan lectus. Nulla facilisi. Maecenas egestas pretium erat, non bibendum metus facilisis sit amet. Integer faucibus in sapien eget aliquam. Maecenas mauris orci, pretium non condimentum non, fringilla quis justo. Nam ultricies, metus et egestas gravida, eros magna finibus turpis, quis posuere mauris felis nec augue. Nullam dui ipsum, faucibus a feugiat sit amet, pretium cursus turpis."
 
+
+
 /*
 $(document).on("click", ".home", function(){
     console.log("home clicked");
