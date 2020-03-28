1bec3bbee31431d51ac07304569ce4fc06f8b570
swechchhaparajuli <swechchha@gmail.com>
2020-03-28 09:21:16 -0700

    aboutme and resume buttons added

--- js/styles.js
+++ js/styles.js
@@ -3,20 +3,21 @@
 var profileDescription = document.getElementById("profile-description");
 var profileImage = document.getElementById("profile-image");
 
-profileDescription.textContent = " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ante sem, lacinia eget ante sit amet, tincidunt ultricies velit. Fusce quis sagittis dui. Cras lobortis ipsum urna, sit amet molestie ligula condimentum vitae. Sed feugiat urna vitae leo ultrices finibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In hac habitasse platea dictumst. Pellentesque mi massa, viverra at rutrum et, gravida eu metus. Nam hendrerit nisl id erat tincidunt, vitae rhoncus odio fringilla. Suspendisse id nibh id ex faucibus ultrices. Aenean tincidunt mattis massa vel commodo. Nulla at nisi cursus, cursus enim vitae, tempus velit. Nulla facilisi." +
-
-"Vivamus a ullamcorper velit. Pellentesque lorem orci, tincidunt sit amet dui a, lacinia imperdiet lacus. Vivamus at ex eget diam placerat venenatis non et tortor. Aenean lacinia venenatis augue, vitae convallis justo consectetur vel. Vivamus quis vestibulum ex. Donec ante neque, vestibulum nec lorem a, porttitor pretium elit. Cras ac nisl elit.\n" +
-
-"Donec sagittis massa non mauris aliquet, sit amet rhoncus orci facilisis. Donec a odio nunc. Suspendisse in aliquet diam. Etiam accumsan scelerisque enim eget suscipit. Curabitur commodo metus eget tristique imperdiet. Aliquam malesuada enim libero, in laoreet nisl tincidunt dapibus. Sed finibus non orci eu egestas. Sed dapibus velit vitae suscipit ornare. Donec est massa, imperdiet sagittis dolor vel, luctus accumsan lectus. Nulla facilisi. Maecenas egestas pretium erat, non bibendum metus facilisis sit amet. Integer faucibus in sapien eget aliquam. Maecenas mauris orci, pretium non condimentum non, fringilla quis justo. Nam ultricies, metus et egestas gravida, eros magna finibus turpis, quis posuere mauris felis nec augue. Nullam dui ipsum, faucibus a feugiat sit amet, pretium cursus turpis."
 
 
-
-/*
-$(document).on("click", ".home", function(){
-    console.log("home clicked");
-    window.location.href =  "./index.html";
+$(document).on("click", "#mebtn", function(){
+    console.log("me clicked");
+    $("#page3").css("height", "750px");
+    $("#page3").css("padding-top", "50px");
 }); 
 
+$(document).on("click", "#resbtn", function(){
+    console.log("me clicked");
+    $("#page4").css("height", "750px");
+    $("#page4").css("padding-top", "50px");
+}); 
+
+/*
 $(document).on("click", ".contact", function(){
     window.location.href =  "./contact.html";
 });
