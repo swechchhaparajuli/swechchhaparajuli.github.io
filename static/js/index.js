b0bb828288ae985a336192463f41209002da6bd7
swechchhaparajuli <swechchha@gmail.com>
2020-06-29 21:14:59 -0700

    edited sponsor a child

--- static/js/index.js
+++ static/js/index.js
@@ -2,16 +2,14 @@ var profileDescription = document.getElementById("profile-description");
 var profileImage = document.getElementById("profile-image");
 
 $(document).on("click", "#mebtn", function(){
-    console.log("me clicked");
     $("#page3").css("height", "750px");
     $("#page3").css("padding-top", "50px");
 }); 
 
 $(document).on("click", "#resbtn", function(){
-    console.log("me clicked");
     $("#page4").css("height", "750px");
     $("#page4").css("padding-top", "50px");
-}); 
+});
 
 // vue
 
